938d4b3ece91eba1bb7123928e41925df3ef36af
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-30 10:28:14 +0200

    Add MoviesContext

---
+++ EfCodeFirst.DataAccess/MoviesContext.cs
@@ -0,0 +1,29 @@
+﻿using EfCodeFirst.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirst.DataAccess;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Gender> Genders { get; set; }
+    public DbSet<Person> Persons { get; set; }
+
+    public MoviesContext()
+    { }
+
+    public MoviesContext(DbContextOptions<MoviesContext> options)
+        : base(options)
+    { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(Constants.ConnectionString);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Gender>()
+            .HasMany(g => g.Persons)
+            .WithOne(p => p.Gender)
+            .HasForeignKey(p => p.GenderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
