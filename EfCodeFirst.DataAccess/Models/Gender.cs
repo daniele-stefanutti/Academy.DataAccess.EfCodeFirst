fce75948041de11549f9c8e4543925f8d321d669
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-30 10:27:50 +0200

    Add models

---
+++ EfCodeFirst.DataAccess/Models/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EfCodeFirst.DataAccess.Models;
+
+public class Gender
+{
+    [Key]
+    [Column(name: "gender_id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int Id { get; set; }
+
+    [Column(name: "gender")]
+    [Required]
+    [MaxLength(20)]
+    public string Name { get; set; } = string.Empty;
+
+    public virtual ICollection<Person> Persons { get; } = Array.Empty<Person>();
+}
---
+++ EfCodeFirst.DataAccess/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EfCodeFirst.DataAccess.Models;
+
+public class Person
+{
+    [Key]
+    [Column(name: "person_id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PersonId { get; set; }
+
+    [Column(name: "gender_id")]
+    [Required]
+    public int GenderId { get; set; }
+
+    [Column(name: "person_name")]
+    [Required]
+    [MaxLength(500)]
+    public string PersonName { get; set; } = string.Empty;
+
+    public virtual Gender Gender { get; set; } = null!;
+}
