960e4c219cb2af1421b4326be5007522bb426d8e
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-30 10:27:00 +0200

    Add Constants

---
+++ EfCodeFirst.DataAccess/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace EfCodeFirst.DataAccess;
+
+internal static class Constants
+{
+    public const string ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=Movies;Integrated Security=True;Encrypt=False";
+}
