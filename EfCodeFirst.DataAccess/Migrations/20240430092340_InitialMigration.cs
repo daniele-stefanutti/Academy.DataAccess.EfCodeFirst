767df41b7720c4fb8e03c2f365908d0987d10b25
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-30 11:24:13 +0200

    Add initial migration

---
+++ EfCodeFirst.DataAccess/Migrations/20240430092340_InitialMigration.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EfCodeFirst.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    gender_id = table.Column<int>(type: "int", nullable: false),
+                    gender = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.gender_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    person_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    gender_id = table.Column<int>(type: "int", nullable: false),
+                    person_name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.person_id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Genders_gender_id",
+                        column: x => x.gender_id,
+                        principalTable: "Genders",
+                        principalColumn: "gender_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "gender_id", "gender" },
+                values: new object[,]
+                {
+                    { 1, "Female" },
+                    { 2, "Male" },
+                    { 3, "Unspecified" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_gender_id",
+                table: "Persons",
+                column: "gender_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
