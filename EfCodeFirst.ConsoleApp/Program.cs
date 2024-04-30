4fd51018610257bbefa3d608f0f22b7af594c69b
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-30 10:28:43 +0200

    Add console application

---
+++ EfCodeFirst.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using EfCodeFirst.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Application started");
+
+        MoviesContext context = new(new DbContextOptions<MoviesContext>());
+        await context.Database.MigrateAsync();
+    }
+}
