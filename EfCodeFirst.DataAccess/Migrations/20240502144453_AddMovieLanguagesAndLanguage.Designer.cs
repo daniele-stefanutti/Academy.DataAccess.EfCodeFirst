09b2199e76abe2540045a8beb740ce7483fae214
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-05-02 16:46:37 +0200

    Add Migration AddMovieLanguagesAndLanguage

--- EfCodeFirst.DataAccess/Migrations/20240502144453_AddMovieLanguagesAndLanguage.Designer.cs
+++ EfCodeFirst.DataAccess/Migrations/20240502144453_AddMovieLanguagesAndLanguage.Designer.cs
@@ -4,6 +4,7 @@ using EfCodeFirst.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EfCodeFirst.DataAccess.Migrations
 {
     [DbContext(typeof(MoviesContext))]
-    partial class MoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20240502144453_AddMovieLanguagesAndLanguage")]
+    partial class AddMovieLanguagesAndLanguage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EfCodeFirst.DataAccess/Migrations/20240502144453_AddMovieLanguagesAndLanguage.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EfCodeFirst.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieLanguagesAndLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    language_id = table.Column<int>(type: "int", nullable: false),
+                    language_code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    language_name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.language_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie_Languages",
+                columns: table => new
+                {
+                    movie_id = table.Column<int>(type: "int", nullable: false),
+                    language_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie_Languages", x => new { x.movie_id, x.language_id });
+                    table.ForeignKey(
+                        name: "FK_Movie_Languages_Language_language_id",
+                        column: x => x.language_id,
+                        principalTable: "Language",
+                        principalColumn: "language_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Movie_Languages_Movie_movie_id",
+                        column: x => x.movie_id,
+                        principalTable: "Movie",
+                        principalColumn: "movie_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Language",
+                columns: new[] { "language_id", "language_code", "language_name" },
+                values: new object[,]
+                {
+                    { 1, "en", "English" },
+                    { 2, "es", "Spanish" },
+                    { 3, "de", "German" },
+                    { 4, "it", "Italian" },
+                    { 5, "ja", "Japanese" },
+                    { 6, "hi", "Hindi" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_Languages_language_id",
+                table: "Movie_Languages",
+                column: "language_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie_Languages");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+        }
+    }
+}
